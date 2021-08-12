872819a6df6f22abb7ec0bd3ec5fa3d8b14dff42
Ava Choi <alexbchoi@gmail.com>
2021-08-12 15:39:01 -0400

    routes2

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const morgan = require('morgan');
-const express = require('express');
+const morgan = require("morgan");
+const express = require("express");
 const app = express();
-const {addPage, editPage, index, layout, main, userList, userPages, wikipage} = require('./jsmodules');
-app.use(morgan);
+const layout = require("./views/layout");
+const { db } = require("./models");
+
+app.use(morgan("dev"));
 
 // const expressStaticFiles = express.static(path.join(__dirname, 'public'))
 // app.use(expressStaticFiles);
 
-app.use(express.static(__dirname +
-  "/public"));
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 //this route will catch all requests, and add req.body to each request; - anonymous
 
-app.get('/', (req,res) => {
-  res.send("hello world!")
-})
+app.get("/", (req, res) => {
+	res.send(layout(""));
+});
+
+db.authenticate().then(() => {
+	console.log("connected to the database");
+});
 
 const PORT = 3000;
-app.listen(PORT,()=>{
-  console.log(`user listening ${PORT}`)
-})
-
-
+app.listen(PORT, () => {
+	console.log(`user listening ${PORT}`);
+});
