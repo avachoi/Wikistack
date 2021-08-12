2f363b8ec26d8750ca78149fc45713d50b96a077
Ava Choi <alexbchoi@gmail.com>
2021-08-12 18:07:16 -0400

    sending the page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,28 +1,34 @@
-
-const express = require('express');
+const express = require("express");
 const route = express.Router();
 const { db, Page, User } = require("../models/index");
 const addPage = require("../views/addPage");
-const slugify = require('slugify');
+const slugify = require("slugify");
+const wikiPage = require("../views/wikiPage");
 
-  route.get('/', async (req, res) => {
-    let pages = await Page.findAll();
-    res.send(pages);
-  })
+route.get("/", async (req, res) => {
+	let pages = await Page.findAll();
+	res.send(pages);
+});
 
-  route.post('/', async (req, res) => {
+route.post("/", async (req, res) => {
+	const newPage = await Page.create({
+		title: req.body.title,
+		slug: slugify(req.body.title),
+		content: req.body.content,
+		status: req.body.status,
+	});
+	res.json(newPage);
+});
 
-    const newPage = await Page.create({
-      title:req.body.title,
-      slug:slugify(req.body.title),
-      content:req.body.content,
-      status:req.body.status
-    })
-    res.json(newPage)
-  })
+route.get("/add", async (req, res) => {
+	res.send(addPage());
+});
 
-  route.get('/add', async (req, res) => {
-    res.send(addPage())
-  })
+route.get("/:slug", async (req, res, next) => {
+	const foundPage = await Page.findOne({
+		where: { slug: req.params.slug },
+	});
+	res.send(wikiPage(foundPage));
+});
 
-module.exports = route
+module.exports = route;
